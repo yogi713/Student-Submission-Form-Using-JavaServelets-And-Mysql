badff423b0298b3fa3c4545d59f1e821f9dd6ac5
Yogi <103526371+yogi713@users.noreply.github.com>
2023-05-10 22:01:07 +0530

    Update LoginServlet.java

--- LoginServlet.java
+++ LoginServlet.java
@@ -1,6 +1,4 @@
 package Servlet;
-import java.io.IOException;
-import java.io.PrintWriter;
 import java.sql.*;
 
 import javax.servlet.RequestDispatcher;
@@ -9,24 +7,33 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
 @WebServlet("/LoginServlet")
 public class LoginServlet extends HttpServlet {
-	protected void doPost(HttpServletRequest req, HttpServletResponse res)
-			throws ServletException, IOException {
+	protected void doPost(HttpServletRequest req, HttpServletResponse res){
 		try {
-//			Class.forName("com.mysql.jdbc.Driver");
-			PrintWriter print = res.getWriter();
 			String url = "jdbc:mysql://localhost:3306/server";
 			String username = "root";
-			String password = "YOGIcr7123";
+			String passWord = "YOGIcr7123";
 			Connection connection =
-					DriverManager.getConnection(url, username, password);
-			String n = req.getParameter("name");
-			String p = req.getParameter("password");
-			String query = "insert into numbers values(?,?)";
+					DriverManager.getConnection(url, username, passWord);
+			String name = req.getParameter("name");
+			String password = req.getParameter("password");
+			String email = req.getParameter("email");
+			String sms = req.getParameter("sms");
+			String date = req.getParameter("date");
+			String gender = req.getParameter("gender");
+			String state = req.getParameter("state");
+			String queries = req.getParameter("queries");
+			String query = "insert into details values(?,?,?,?,?,?,?,?)";
 			PreparedStatement pst = connection.prepareStatement(query);
-			pst.setString(1,n);
-			pst.setString(2,p);
+			pst.setString(1,name);
+			pst.setString(2,password);
+			pst.setString(3,email);
+			pst.setString(4,sms);
+			pst.setString(5,date);
+			pst.setString(6,gender);
+			pst.setString(7,state);
+			pst.setString(8,queries);
 			pst.executeUpdate();
-			RequestDispatcher rd = req.getRequestDispatcher("success.jsp");
+			RequestDispatcher rd = req.getRequestDispatcher("success.html");
 			rd.forward(req, res);
 		} catch (Exception e) {e.printStackTrace();}
 	}
